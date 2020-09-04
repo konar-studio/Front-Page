6d9160456d1e6a76b3a5e32d333dddad23b1f77c
Ali Zemani <mehot1@gmail.com>
2020-09-05 02:16:30 +0430

    Site updated: 2020-09-05 02:16:30

--- solutions/surface-scanner/wapp/build/worker.js
+++ solutions/surface-scanner/wapp/build/worker.js
@@ -10,18 +10,40 @@ onmessage = async ({
 
   if (messageType === "load") {
     postMessage('Worker loading');
-    server = await wsConnect();
 
-    if (!WebAssembly.instantiateStreaming) { // polyfill
-      WebAssembly.instantiateStreaming = async (resp, importObject) => {
-        const source = await (await resp).arrayBuffer();
-        return await WebAssembly.instantiate(source, importObject);
-      };
+    let server = new EventSource('https://signaling.local/hello');
+    console.log(server.withCredentials)
+
+    server.onmessage = function (event) {
+      console.log(event);
+    };
+    server.onerror = function (event) {
+      console.log("SSE onerror ");
+      console.log(event);
+      console.log(event.target.readyState);
+      console.log(server);
+    }
+    server.onopen = function () {
+      console.log(server);
+      console.log("SSE onopen");
     }
 
-    WebAssembly.instantiateStreaming(fetch(wasm_url), go.importObject).then((result) => {
-      go.run(result.instance);
-    });
+
+
+
+    // server = await wsConnect();
+    // server = await eventSourceConnect();
+
+    // if (!WebAssembly.instantiateStreaming) { // polyfill
+    //   WebAssembly.instantiateStreaming = async (resp, importObject) => {
+    //     const source = await (await resp).arrayBuffer();
+    //     return await WebAssembly.instantiate(source, importObject);
+    //   };
+    // }
+
+    // WebAssembly.instantiateStreaming(fetch(wasm_url), go.importObject).then((result) => {
+    //   go.run(result.instance);
+    // });
 
     postMessage('Wasm loading');
   }
@@ -58,6 +80,27 @@ const wsConnect = async () => {
     server.onerror = function (err) {
       reject(err);
     };
+  });
+}
+const eventSourceConnect = async () => {
+  return new Promise(function (resolve, reject) {
+
+    let server = new EventSource('https://signaling.local/hello');
+
+    server.addEventListener('open', function (e) {
+      console.log("Events Connected");
+      resolve(server);
+    }, false);
+
+    server.addEventListener('error', function (err) {
+      if (err.target.readyState != EventSource.OPEN) {
+        console.log("Events Disconnected");
+        reject(err);
+      }
+    }, false);
+    server.addEventListener('message', function (e) {
+      console.log("message", e.data);
+    }, false);
   });
 }
 
